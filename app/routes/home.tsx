239763395ae2e891e51b368aefd06a1f574a3208
Miguel Palau Zarza <mpalauzarza@gmail.com>
2025-03-01 07:24:01 -0600

    better defaults

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,9 @@
+const message = `hello world, try searching on '/api/download'`;
+
 export const loader = async () => {
-  return { ok: true, message: "hello world, try searching on `/api/download`" };
+  return { ok: true, message };
 };
+
+export default function Home() {
+  return message;
+}
