36f74e3a9b7b49bc68e4cebcbfd23f87ae0747bf
Miguel Palau Zarza <mpalauzarza@gmail.com>
2025-03-02 13:32:33 -0600

    simplify code

--- app/routes/api.download.tsx
+++ app/routes/api.download.tsx
@@ -1,21 +1,11 @@
 import type { Route } from "./+types/api.download";
-import { downloader } from "~/utils/downloader";
 import { data } from "react-router";
 
-export const loader = async ({ request }: Route.LoaderArgs) => {
-  const url = new URL(request.url);
-  const q = url.searchParams.get("q");
-  if (!q)
-    return data(
-      { ok: false, message: `missing query to download` },
-      { status: 400 }
-    );
-  const chatId = url.searchParams.get("chat_id");
-  const downloadUrl = downloader({ url: q });
-  return { ok: true, q, chatId, downloadUrl };
-};
-
 export const action = async ({ request }: Route.ActionArgs) => {
+  if (request.method !== "POST") {
+    return data({ message: `only POST is allowed` }, { status: 405 });
+  }
+
   const update = await request.json();
   // Handle messages
   if (update.message?.text) {
--- app/utils/downloader.ts
+++
@@ -1,8 +0,0 @@
-interface Downloader {
-  url: string;
-}
-
-export const downloader = (props: Downloader) => {
-  const { url } = props;
-  return url;
-};
