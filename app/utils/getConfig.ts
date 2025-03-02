3612a68646891abd34dffa072f8a316104c12325
Miguel Palau Zarza <mpalauzarza@gmail.com>
2025-03-02 13:27:55 -0600

    we can see the config now

--- app/utils/getConfig.ts
+++ app/utils/getConfig.ts
@@ -1,28 +1,15 @@
-import "dotenv/config";
+import config from "config";
 
-export interface Config {
+export type Config = {
   token: string;
   serverUrl: string;
   preferredVideoExtension?: string;
-}
+};
 
 export function getConfig(): Config {
-  const config: Partial<Config> = {};
-
-  // Helper function to convert camelCase to snake_case
-  function camelToSnakeCase(key: string): string {
-    return key.replace(/[A-Z]/g, (letter) => `_${letter.toLowerCase()}`);
-  }
-
-  // Iterate over the keys of the Config interface
-  (Object.keys(config) as (keyof Config)[]).forEach((key) => {
-    const envKey = camelToSnakeCase(key).toUpperCase(); // Convert camelCase to snake_case and uppercase
-    if (process.env[envKey]) {
-      config[key] = process.env[envKey] as string;
-    } else {
-      throw new Error(`Missing environment variable: ${envKey}`);
-    }
-  });
-
-  return config as Config;
+  return {
+    token: config.get("TG_BOT_TOKEN"),
+    serverUrl: config.get("SERVER_URL"),
+    preferredVideoExtension: config.get("PREFERRED_VIDEO_EXTENSION"),
+  };
 }
