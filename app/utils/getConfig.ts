cd170f3ce6aeaacef6b5bc063643c774f763d5e5
Miguel Palau Zarza <mpalauzarza@gmail.com>
2025-03-02 19:22:42 -0600

    add a default mediaDir

--- app/utils/getConfig.ts
+++ app/utils/getConfig.ts
@@ -4,12 +4,24 @@ export type Config = {
   token: string;
   serverUrl: string;
   preferredVideoExtension?: string;
+  mediaDir?: string;
+};
+
+const defaultConfig: Partial<Config> = {
+  preferredVideoExtension: "mp4",
+  mediaDir: "/share",
 };
 
 export function getConfig(): Config {
-  return {
+  const userConfig: Config = {
     token: config.get("TG_BOT_TOKEN"),
     serverUrl: config.get("SERVER_URL"),
     preferredVideoExtension: config.get("PREFERRED_VIDEO_EXTENSION"),
+    mediaDir: config.get("MEDIA_DIR"),
+  };
+
+  return {
+    ...defaultConfig,
+    ...userConfig,
   };
 }
